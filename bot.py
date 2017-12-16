eb761783a5d28b14d31621985b266a48b1a5b0c5
cryptonov <34605931+cryptonov@users.noreply.github.com>
2017-12-16 19:55:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=['text'])
 def repeat_all_messages(message):
     print(message)
-    if "мотивац" in message.text.lower():
+    if "мотив" in message.text.lower():
       bot.send_message(message.chat.id,config.citatki[random.randint(0,len(config.citatki))])
     if message.from_user.first_name in config.lox:
         bot.send_message(message.chat.id, "Завали ебало валет!")
