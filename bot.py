54769d4b63232ef66c2ae5c015b883d9a3fadb04
cryptonov <34605931+cryptonov@users.noreply.github.com>
2017-12-17 10:51:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,26 @@
 import config
 import telebot
 import random
+import json
+import urllib2
 
 bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=['text'])
 def repeat_all_messages(message):
     print(message)
+    req = urllib2.Request("https://blockchain.info/ru/ticker")
+    opener = urllib2.build_opener()
+    f = opener.open(req)
+    js = json.loads(f.read())
+    print(json)
     if "мотив" in message.text.lower():
       bot.send_message(message.chat.id,config.citatki[random.randint(0,len(config.citatki)-1)])
     if message.from_user.first_name in config.lox:
         bot.send_message(message.chat.id, "Завали ебало валет!")
     if "парень который лидирует" in message.text.lower():
       bot.send_message(message.chat.id,"В данный момент лидер - Андрон")
+   # if "/биткоин" in message.text.lower():
+   #    bot.send_message(message.chat.id,"Такс такс такс, по данным Кирюхи биток щас %f $"%btc)
 
 
 
