86271aeaef4cea0b8750dda57b76d4977b8f4507
cryptonov <34605931+cryptonov@users.noreply.github.com>
2018-01-14 22:03:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ def repeat_all_messages(message):
         bot.send_message(message.chat.id, "Ты шо вообще тупой? Какой беткоин?")
     if "парень который лидирует" in message.text.lower():
         bot.send_message(message.chat.id, "В данный момент лидер - Андрон")
-    if "славаукраине"||"славаукраїні" in message.text.lower().replace(" ",""):
+    if "славаукраине"|"славаукраїні" in message.text.lower().replace(" ",""):
          bot.send_message(message.chat.id, "Героям слава!!")
          bot.send_sticker(message.chat.id, "CAADAgADHAADMVD5DfMjhuKC0-w7Ag")
 
